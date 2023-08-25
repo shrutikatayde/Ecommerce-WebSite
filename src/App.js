8ee06155aa793137eff51a2a9069fc236344b010
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-08-25 13:45:59 +0530

    Ecommerce Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { Checkbox } from '@mui/material';
+import { useDebugValue, useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
+import Home from "./scenes/home/Home";
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import CartMenu from './scenes/global/CartMenu';
+
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Navbar/>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="ckeckout" element={<Checkout />} />
+          <Route path="ckeckout/success" element={<Confirmation />} />
+        </Routes>
+        <CartMenu/>
+      </BrowserRouter >
+
     </div>
   );
 }
